b7c0ace4b72e6af7fb6856e584563a5390ea6d48
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-11 18:00:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,12 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
+  elsif int % 5 == log10
+    "Buzz"
+  end
   elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+  end
+else 
+  "nil"
 end
